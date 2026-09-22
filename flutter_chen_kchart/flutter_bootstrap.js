2d499460a383a353357c5ae964bc548fd41b9712
Victor <12517526480qq.com>
2026-09-22 14:32:26 +0800

    feat: kchart demo

--- flutter_chen_kchart/flutter_bootstrap.js
+++ flutter_chen_kchart/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1554076309"
+    serviceWorkerVersion: "1593627732"
   }
 });
